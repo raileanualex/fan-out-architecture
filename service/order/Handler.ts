6d084e0409c2b2449055ce5e41be78c7d4f973b5
Alexandru Raileanu <net.alexandru.raileanu@mhp.com>
2024-07-11 23:54:25 +0300

    feat: add logs

--- service/order/Handler.ts
+++ service/order/Handler.ts
@@ -4,20 +4,26 @@ import { OrderAdapter } from "./Adapter";
 import { OrderRepository } from "./OrderRepository";
 
 async function initialize(): Promise<OrderAdapter> {
+    console.log("LOG=INITIALIZE LAMBDA1");
     const orderRepository = new OrderRepository();
+    console.log("LOG=INITIALIZE LAMBDA4");
     const usecase = new Order(
         orderRepository
     );
-
+    console.log("LOG=INITIALIZE LAMBDA2");
     return new OrderAdapter(usecase);
 }
 
 let adapter: OrderAdapter;
 
 export async function handler(event: SQSEvent, context: Context): Promise<void> {
+    console.log("LOG=INITIALIZE LAMBDA0");
+
     if (!adapter) {
         adapter = await initialize();
     }
+    console.log("LOG=INITIALIZE LAMBDA3");
+
     adapter.handleEvent(event, context);
 }
 
