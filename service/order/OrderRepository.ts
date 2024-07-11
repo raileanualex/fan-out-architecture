10a4429c3769746fd4ad737a929d23eef0c9e0af
Alexandru Raileanu <net.alexandru.raileanu@mhp.com>
2024-07-12 01:46:35 +0300

    feat: add aws sns client

--- service/order/OrderRepository.ts
+++ service/order/OrderRepository.ts
@@ -1,3 +1,4 @@
+import * as sns from "@aws-sdk/client-sns";
 import { internalServerError } from "@enter-at/lambda-handlers";
 
 export interface Repository {
@@ -24,11 +25,10 @@ export class OrderRepository implements Repository {
                 throw new Error("ORDER_TOPIC_ARN environment variable is not set");
             }
 
-            // const snsClient = new sns.SNSClient({ region: 'eu-central-1' });
-            // console.log("LOG=after create sns client", JSON.stringify(snsClient));
+            const snsClient = new sns.SNSClient({ region: 'eu-central-1' });
 
-            // const response = await snsClient.send(new sns.PublishCommand(params));
-            // console.log("LOG=after publish", JSON.stringify(response));
+            const response = await snsClient.send(new sns.PublishCommand(params));
+            console.log("LOG=after publish", JSON.stringify(response));
 
             return {
                 statusCode: "Successful",
